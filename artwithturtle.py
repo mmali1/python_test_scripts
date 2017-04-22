6e7dd86c328c9a1182ac1f05788bd8a940a76d61
mmali1 <meenakshi20mali@gmail.com>
2017-04-21 18:44:29 -0700

    practiced python's turtle model for drawings

---
+++ artwithturtle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import turtle
+
+def draw_square(square_turtle):
+	for i in range(4):
+		square_turtle.forward(100)
+		square_turtle.right(90)
+
+
+def draw_circle(circle_turtle) :
+	circle_turtle.circle(100)
+
+
+def draw_triangle(tri_turtle):
+
+	tri_turtle.right(30)
+	tri_turtle.forward(100)
+	tri_turtle.right(60)
+	tri_turtle.forward(100)
+	tri_turtle.left(30)
+	tri_turtle.backward(200)
+
+
+def draw_rhombus(go_turtle):
+	for i in range(2):
+		go_turtle.forward(100)
+		go_turtle.left(45)
+		go_turtle.forward(100)
+		go_turtle.left(135)
+
+
+def main():
+	window = turtle.Screen()
+	window.bgcolor("#FFFAF0")
+
+	myTurtle = turtle.Turtle()
+	myTurtle.shape("turtle")
+	myTurtle.color("#9ACD32")
+
+	for i in range(19):
+		draw_square(myTurtle)
+		myTurtle.right(20)
+
+	window.clear()
+
+	for i in range(38):
+		draw_rhombus(myTurtle)
+		myTurtle.right(10)
+
+#	draw_circle(myTurtle)
+#	draw_triangle(myTurtle)
+
+	window.exitonclick()
+
+
+if __name__ == '__main__':
+  main()
+
+
